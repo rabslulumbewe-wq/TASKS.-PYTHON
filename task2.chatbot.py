2d36ef30c69be539f373fd74b8a7e943a28c21ee
rabslulumbewe-wq <rabslulumbewe@gmail.com>
2026-09-03 13:15:04 -0800

    Add files via upload

---
+++ task2.chatbot.py
@@ -0,0 +1,13 @@
+name = input ("whats your name?")
+
+print (f"Hey {name}! nice to meet you.")
+
+feeling = input("How are you keeping up today? ")
+
+if feeling == "good":
+    
+        print("i know thats right")
+              
+elif feeling == "bad":
+    
+        print("sorry to hear that, collegue")
