5c3db512e625c9d221f4ea4bbbe2c6f7729a1c06
rabslulumbewe-wq <rabslulumbewe@gmail.com>
2026-09-03 13:14:28 -0800

    Add files via upload

---
+++ task1_calulate_age.py
@@ -0,0 +1,11 @@
+name = input("whats your name")
+
+birth_year = input("when is your birthday")
+
+birth_year = int(birth_year)
+
+current_year = 2026
+
+age = current_year - birth_year
+
+print(f"Hello {name}, you are {age} years old")  
